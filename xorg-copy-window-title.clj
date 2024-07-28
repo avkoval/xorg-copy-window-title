3fb62f19342ef9571a3916c56d22a557167aca77
Oleksii Koval <alex@koval.kharkov.ua>
2024-07-28 08:19:11 +0300

    Initial commit: copy window title (filtering - Jira*)

---
+++ xorg-copy-window-title.clj
@@ -0,0 +1,50 @@
+#!/usr/bin/env bb
+(ns xorg-copy-window-title
+  (:require
+   [babashka.process :refer [shell process]]
+   [clojure.string :as str]
+   [clojure.java.io :as io]
+   )
+  )
+
+(defn xwininfo [id]
+  (into {} (remove nil?
+                   (for [line (-> (shell {:out :string} (str "xwininfo -id " id))
+                                  :out
+                                  str/split-lines
+                                  )]
+                     (let [index_of_colon (str/index-of line ":")] 
+                       (cond 
+                         (str/starts-with? line "xwininfo: Window id:") {:title (str/replace (subs line 32) "\"" "")}
+                         (not (nil? index_of_colon)) (hash-map (str/trim (subs line 0 index_of_colon)) (str/trim (subs line (+ 1 index_of_colon))))
+                         ))
+                     )))
+  )
+
+(defn get-active-window []
+  (str "0x" (str/replace (format "%8x" (Integer/parseInt (str/trim (:out (shell {:out :string} (str "xdotool getwindowfocus")))))) #"\s" "0"))
+  )
+
+(comment
+  (let [title "[FH-9202] BE. Implement jQuery solution for Django template HTML object display - Jira - Google Chrome"
+        filtered (str/replace title #" - Jira.*" "")
+        ]
+    filtered
+    )
+  )
+
+
+(defn -main [& _]
+  (let [clipboard (process "xclip -selection clipboard")
+        clipboard_in (io/writer (:in clipboard)) 
+        title (:title (xwininfo (get-active-window)))
+        no_jira_title (str/replace title #" - Jira.*" "")
+        ]
+    (binding [*out* clipboard_in]
+      (println no_jira_title))
+    (.close clipboard_in)
+    )
+  )
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (apply -main *command-line-args*))
